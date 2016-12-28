d22ee2c2df1662ac329c5733f54a988890bb6417
chengjunwen <chengjunwen1965@163.com>
2016-12-28 20:30:44 +0800

    noiseautoencoder

--- autoencoder.py
+++ autoencoder.py
@@ -8,10 +8,18 @@ import matplotlib.pyplot as plt
 def _get_data():
     mnist = input_data.read_data_sets('MNIST_data',one_hot=True)
     return mnist
-def autoencoder(input,numOuts=[784,256,64]):
+def get_noise_image(input, noise_ratio):
+### denoise
+    noise_image = np.random.uniform(-0.5,0.5,
+                        (input.get_shape())).astype('float32')
+    return noise_ratio*noise_image + (1-noise_ratio)*input
+    
+    
+def autoencoder(input,numOuts=[784,256,64],noise_ratio=0.5):
     
     encoder =[]
-    current_x = input
+    current_x = get_noise_image(input, noise_ratio)
+
     for l, n_out in enumerate(numOuts[1:]):
         n_in = int(current_x.get_shape()[1]);
         shape = [n_in, n_out]
@@ -39,12 +47,23 @@ def autoencoder(input,numOuts=[784,256,64]):
 
     return (y,loss)
 
+def show_img(test_data,test_y, mean_img,nimgs=10):
+    fig, axs = plt.subplots(2, 10, figsize=(10, 2))
+    for i in range(nimgs):
+        axs[0][i].imshow(
+            np.reshape(test_data[i, :], (28, 28)))
+        axs[1][i].imshow(
+            np.reshape([test_y[i, :] + mean_img], (28, 28)))
+
+    fig.show()
+    plt.draw()
+    plt.waitforbuttonpress()
+
 def train_mnist():
     mnist = _get_data()
     mean_img = np.mean(mnist.train.images, axis=0)
-
     batch_size = 64
-    epochs = 50
+    epochs = 2
     lr = 0.003
     x = tf.placeholder(tf.float32, shape=[batch_size, 784])
     y, loss =  autoencoder(x)
@@ -56,10 +75,14 @@ def train_mnist():
     for epoch in range(epochs):
         for i in range(mnist.train.num_examples//batch_size):
             batch = mnist.train.next_batch(batch_size)[0]
-            train_x = np.array([img - mean_img for img in batch])
+            train_x = np.array([(img - mean_img) for img in batch])
             y_img,loss_y,_ = sess.run([y,loss,optimizer], feed_dict = {x:train_x})
 
         print("epoch: %d ,loss: %f" % (epoch,loss_y))
 
-       
+### test
+    test_data = mnist.test.next_batch(batch_size)[0]
+    test_x = np.array([(img - mean_img) for img in test_data])
+    test_y = sess.run(y,feed_dict={x:test_x})
+    show_img(test_data, test_y,mean_img)
 train_mnist()
